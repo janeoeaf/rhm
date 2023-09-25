e120dea8c261e6d68fdf4a289cde5c76bc11d4bf
janeoeaf <janeoeaf@gmail.com>
2023-09-25 18:42:19 +0000

    add covariate and make grm

--- 0_download_files.R
+++ 0_download_files.R
@@ -8,5 +8,12 @@ snp=aws.s3::s3read_using(FUN=readr::read_table,
                          object = 'jaime/input/geno.hmp.txt',
                          bucket = Sys.getenv('bucket'))
 
+covar=aws.s3::s3read_using(FUN=readr::read_table,
+                           object = 'jaime/input/covar.txt',
+                           bucket = Sys.getenv('bucket'))
+  
+
+
 readr::write_tsv(pheno,'tmp/pheno.txt')
 readr::write_tsv(snp,'tmp/geno.hmp.txt')
+readr::write_tsv(covar,'tmp/covar.txt')
---
+++ 2_make_grm.R
@@ -0,0 +1,34 @@
+rm(list=ls())
+library(snpStats)
+library(sommer)
+w<-.99
+
+
+gp=read.plink('tmp/geno')
+windown_rhm<-readr::read_csv('tmp/window.csv')
+map0=gp$map %>% arrange(chromosome,position)
+
+map<-data.frame(chr=map0$chr, snpID=rownames(map0),cM=0, pos=map0$pos)
+
+snp<-as(gp$geno,'numeric')-1
+snp=snp[,map$snpID]
+GRM<-w*A.mat(snp)+diag(nrow(snp))*(1-w)
+
+
+
+##############
+#GRM regional#
+##############
+GRMlist<-list()
+for(i in 1:nrow(windown_rhm)){
+  windi<-windown_rhm[i,]
+  
+  snp0_order<-which(colnames(snp)==as.character(windi[1,1]))
+  snp1_order<-which(colnames(snp)==as.character(windi[1,2]))
+  
+  snpr<-as(gp$geno[,snp0_order:snp1_order],'numeric')-1
+  if(ncol(snpr)!=windi[1,6]) stop('erro 123')
+  Gr<-w*A.mat(snpr)+diag(nrow(snpr))*(1-w)
+  GRMlist[[length(GRMlist)+1]]<-Gr
+}
+save(GRM,GRMlist,windown_rhm,file='tmp/grm.Rdata')
